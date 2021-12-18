f9262fc82b291e01c383a1d37f535c0b2c41a153
Patryk Zieliński <patryk.zielinski.stg@gmail.com>
2021-12-18 10:41:50 +0100

    Ćwiczenie 5

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.5 */
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if (age % 3 == 0) {
+            System.out.println("Podzielny przez 3");
+        } else {
+            System.out.println("Niepodzielny przez 3");
+        }
 
-        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
-         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
-         * */
+        int index = scanner.nextInt();
+        System.out.println(index % 2 == 0 ? "Parzysty" : "Nieparzysty");
 
-
-        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
-
-
-        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
-         * i stwórz konstrukcję else if z dowolnymi komunikatami */
-
-
+        double value = scanner.nextDouble();
+        if (value - ((int) value) > 0.5) {
+            System.out.println("Duża wartość dziesiętna");
+        } else {
+            System.out.println("Niska wartość dziesiętna");
+        }
     }
 }
