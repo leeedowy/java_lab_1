c6ce9d7e30754baa04d6a5a176652d96f21a50e0
Patryk Zieliński <patryk.zielinski.stg@gmail.com>
2021-12-18 09:20:23 +0100

    Ćwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-//        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
-
+        System.out.println("Cześć wszystkim!");
+        System.out.println("Przykładowy tekst taki sobie jest");
+        System.out.println("Dwa" + " " + "plus " + "dwa" + " to cztery");
+        System.out.println("Przykłady placeholderów");
+        System.out.printf("Wlzał %s na płotek i %s\n", "kotek", "mruga");
+        System.out.print("Całkiem ");
+        System.out.print("spory ");
+        System.out.print("ten tekst!");
     }
 }
