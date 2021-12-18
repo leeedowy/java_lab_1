9a1a98f6fc9b335873aac77bdd06e8327608746f
Patryk Zieliński <patryk.zielinski.stg@gmail.com>
2021-12-18 09:59:51 +0100

    Ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.4 */
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
 
-        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        System.out.println(a && b && c || a);
+        System.out.println(a && !(b && c || a));
+        System.out.println(b && c || c );
+        System.out.println((b && true) && !c || a);
+        System.out.println((a && b && false || false) || true);
+        System.out.println((a || b || c) && b);
 
+        int x = 53;
+        int y = 23;
+        int z = 62;
 
+        System.out.println(x > z);
+        System.out.println((x == z) || (y < x));
+        System.out.println((y != z) && true);
+        System.out.println((x >= z) || (y <= y) && !(y != z));
     }
 }
